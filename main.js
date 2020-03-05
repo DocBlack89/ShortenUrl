3c30734a73d488abc14940deb9d71c99dae290f6
DocBlack89 <antoinegibier10@hotmail.fr>
2020-03-05 15:45:17 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,14 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const cors = require('cors');
-const mongoose = require('mongoose');
 const errorHandler = require('errorhandler');
-
-//Configure mongoose's promise to global promise
-mongoose.promise = global.Promise;
-
-//Configure isProduction variable
-const isProduction = process.env.NODE_ENV === 'production';
 
 //Initiate our app
 const app = express();
@@ -21,15 +14,7 @@ app.use(require('morgan')('dev'));
 app.use(express.static('./routes/public')); 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(session({ secret: 'passport-tutorial', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 
-
-//Configure Mongoose
-mongoose.connect('mongodb://localhost:32768/passport-tutorial');
-mongoose.set('debug', true);
-
-require('./models/Users');
-require('./config/passport');
 app.use(require('./routes'));
 
 
