2b5670e8302c478aa7ac0a8a886d4f13dc18e5f0
DocBlack89 <antoinegibier10@hotmail.fr>
2020-03-05 11:10:21 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -112,6 +112,40 @@ router.get('/signin', auth.required, function (req, res){
     res.sendFile(path.join(__dirname + '/public/signin.html'));
 })
 
+router.post('/signin', auth.optional, (req, res, next) => {
+    const { body: { user } } = req;
+  
+    if(!user.email) {
+      return res.status(422).json({
+        errors: {
+          email: 'is required',
+        },
+      });
+    }
+  
+    if(!user.password) {
+      return res.status(422).json({
+        errors: {
+          password: 'is required',
+        },
+      });
+    }
+  
+    return passport.authenticate('local', { session: false }, (err, passportUser, info) => {
+      if(err) {
+        return next(err);
+      }
+  
+      if(passportUser) {
+        const user = passportUser;
+        user.token = passportUser.generateJWT();
+  
+        return res.json({ user: user.toAuthJSON() });
+      }
+  
+      return status(400).info;
+    })(req, res, next);
+  });
 
 
 
